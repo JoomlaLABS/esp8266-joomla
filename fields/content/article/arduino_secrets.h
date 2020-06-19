004c479678b6ca4af28642009c10348bc8eeb4b7
Luca Racchetti <razzo1987@gmail.com>
2020-06-19 18:54:57 +0200

    Create arduino_secrets.h

---
+++ fields/content/article/arduino_secrets.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID "your_wifi_username"
+#define WIFI_PASS "your_wifi_password"
+
+#define SITE "your_site_url" // e.g.: test.joomla.com
+// Use web browser to view and copy SHA1 fingerprint of the certificate
+#define FINGERPRINT "your_site_sha1" // e.g.: 97 94 33 AA 4C 52 69 A8 2F D8 E5 D6 EC 29 96 17 94 0C 39 44
+
+#define JOOMLA_USER "your_site_username"
+#define JOOMLA_PASS "your_site_password" // Optional, depends on the configuration of Joomla! Autenticazione API
+#define JOOMLA_TOKEN "your_site_token" // Optional, depends on the configuration of Joomla! Autenticazione API
