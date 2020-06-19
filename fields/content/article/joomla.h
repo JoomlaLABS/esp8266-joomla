517c06f5a4b1d278b1033e6898369a90616e4630
Luca Racchetti <razzo1987@gmail.com>
2020-06-19 18:55:37 +0200

    Create joomla.h

---
+++ fields/content/article/joomla.h
@@ -0,0 +1,21 @@
+void printJoomlaHead() {
+  
+  Serial.println(" f f         i                                                                   ");
+  Serial.println("jiiif        ,,,i                                                                ");
+  Serial.println("fffffjjj i tiiiii                                                                ");
+  Serial.println(" jjjjjffi:iiiiiii         W                                      W              W");
+  Serial.println("  ffj  i,ii :ii           W                                      W              W");
+  Serial.println("  jj  i;iiG  ii           W                                      W              W");
+  Serial.println("  jjfiiiiLLG:ii           W    ;KK        :KK.    t :KG    GK:   W     GKL  ;t  W");
+  Serial.println("  ,j,fii ,LjGii           W  WW.  GWW   WW;  i#W  WWt  WWWW  fW  W  tWW   WWLW  W");
+  Serial.println("   fj;j   LLtL            W WG      WK W#      WW W     WW     W W  W       WW  W");
+  Serial.println("   Gfjtf  GLL;L           W W        W W        W W     WD     W W WK       WW  W");
+  Serial.println("  GGGfjf LGGLG.           W W        W W        W W     WD     W W Wf       LW  W");
+  Serial.println("  GG  f jGG  GG          DW W       KW W       :# W     WD     W W LW       WW   ");
+  Serial.println("  GGG  iGGi  GG.    ;   ,W   Wf    WW   WW    WW  W     WD     W W  WW     WWW  W");
+  Serial.println(" ,,GGt.GGLGGGG,,G   jWWWW     jWWWW      :WWWW:   W     WD     W W    WWWWW fW KW");
+  Serial.println("GLLGGGGG GLGGGLfG                                                                ");
+  Serial.println("LGGGG        LGGG                                                                ");
+  Serial.println(" GGG         jGG                                                                 ");
+  Serial.println("");
+}
